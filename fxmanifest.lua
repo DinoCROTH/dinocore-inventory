1d51214824065fc4e0cc44c5d86b63a21f922d55
DinoCROTH <58315706+DinoCROTH@users.noreply.github.com>
2023-07-18 14:03:27 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,15 +1,13 @@
 fx_version 'cerulean'
 game 'rdr3'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-description 'QBR-Inventory'
-version '1.0.1'
+description 'dinocore-Inventory'
+version '0.0.1'
 
 shared_scripts {
 	'@qbr-core/shared/locale.lua',
    	'locales/en.lua',	
 	'config.lua',
-	-- '@qbr-weapons/config.lua'
 }
 
 server_scripts {
@@ -35,4 +33,3 @@ files {
 }
 
 lua54 'yes'
--- dependency 'qbr-weapons'
